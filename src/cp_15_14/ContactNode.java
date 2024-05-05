44075dfb93eb33a956dd41f35172484714b23e93
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 22:05:25 -0400

    Update ContactNode.java

--- src/cp_15_14/ContactNode.java
+++ src/cp_15_14/ContactNode.java
@@ -1,5 +1,32 @@
 package cp_15_14;
 
 public class ContactNode {
-  /* Type code here. */
+   private String contactName;
+   private String contactPhoneNumber;
+   private ContactNode nextNodePt;
+   
+   public ContactNode(String inputName, String inputNumber) {
+      contactName = inputName;
+      contactPhoneNumber = inputNumber;
+   }
+   
+   public String getName() {
+      return contactName;
+   }
+   public String getPhoneNumber() {
+      return contactPhoneNumber;
+   }
+   public void insertAfter(ContactNode nodeLoc) {
+      ContactNode tmpNext;
+
+      tmpNext = this.nextNodePt;
+      this.nextNodePt = nodeLoc;
+      nodeLoc.nextNodePt = tmpNext;
+   }
+   public ContactNode getNext() {
+      return nextNodePt;
+   }
+   public String toString() {
+      return "Name: " + contactName + "\nPhone number: " + contactPhoneNumber;
+   }
 }
