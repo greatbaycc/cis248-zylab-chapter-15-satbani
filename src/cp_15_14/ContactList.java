f73e9db2c8c43a1e26bf51d5886a05b7ae4c647f
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 22:05:04 -0400

    Update ContactList.java

--- src/cp_15_14/ContactList.java
+++ src/cp_15_14/ContactList.java
@@ -5,7 +5,43 @@ import java.util.Scanner;
 public class ContactList {
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-
-      /* Type your code here. */
+      String userName;
+      String userNumber;
+      ContactNode currNode;
+      
+      System.out.println("Person 1\nEnter name:");
+      userName = scanner.nextLine();
+      System.out.println("Enter phone number:");
+      userNumber = scanner.nextLine();
+      System.out.println("You entered: " + userName + ", " + userNumber + "\n");
+      
+      ContactNode person1 = new ContactNode(userName, userNumber);
+      
+      System.out.println("Person 2\nEnter name:");
+      userName = scanner.nextLine();
+      System.out.println("Enter phone number:");
+      userNumber = scanner.nextLine();
+      System.out.println("You entered: " + userName + ", " + userNumber + "\n");
+      
+      ContactNode person2 = new ContactNode(userName, userNumber);
+      person1.insertAfter(person2);
+      
+      System.out.println("Person 3\nEnter name:");
+      userName = scanner.nextLine();
+      System.out.println("Enter phone number:");
+      userNumber = scanner.nextLine();
+      System.out.println("You entered: " + userName + ", " + userNumber + "\n");
+      
+      ContactNode person3 = new ContactNode(userName, userNumber);
+      person2.insertAfter(person3);
+      
+      System.out.println("CONTACT LIST");
+      currNode = person1;
+      while (currNode != null) {
+         System.out.println(currNode.toString() + "\n");
+         currNode = currNode.getNext();
+      }
+      
+      
    }
 }
